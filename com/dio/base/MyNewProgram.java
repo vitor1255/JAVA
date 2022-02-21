2846120ff372c1302c9ba5fa237409d145a78453
vitor1255 <vitorhmariano9@gmail.com>
2022-02-21 20:47:36 -0300

    first commit

---
+++ com/dio/base/MyNewProgram.java
@@ -0,0 +1,16 @@
+package com.dio.base;
+
+import com.dio.base.Order;
+
+
+public class MyNewProgram {
+    
+public static void main(String[code] args) {
+    Order order = new Order(code: "code1234");
+
+
+    System.out.println(order);
+    
+}
+
+}
